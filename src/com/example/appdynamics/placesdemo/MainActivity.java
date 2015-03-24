c0bc8907d416becad4e97f6b7c6e4ef73c1119e9
Mark Prichard <mqprichard@gmail.com>
2015-03-24 10:40:45 -0700

    Added GooglePlacesKey.java with blank key string

--- src/com/example/appdynamics/placesdemo/MainActivity.java
+++ src/com/example/appdynamics/placesdemo/MainActivity.java
@@ -44,9 +44,9 @@ public class MainActivity extends Activity {
         }
 
         // Enable AppDynamics Instrumentation
-        Instrumentation.start("EUM-AAB-AUA",
+        Instrumentation.start("", // Add EUM Application Key
                               getApplicationContext(),
-                              "http://10.0.2.2:7001",
+                              "", // Add EUM Collector URI
                               true);
     }
 
